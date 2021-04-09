a85346791f9550b614eb6ee5b017594360c3f461
Genesis Tan <genesistanyx@u.nus.edu>
2021-04-09 19:28:41 +0800

    Wrote Codes/Pathways Linking up OrderController to Model(Order.cs) and to View(Index.cshtml) Will need to clarify the use of LINQ or Entity framework to access SQL DBs. The choice will affect the creation of model on myside. For now, i only listed down the parameters needed for OrderController. Will send another request to edit the design of View for Order/Index. Kath&Gen.

--- Team1_ESNET_CA/Models/Order.cs
+++ Team1_ESNET_CA/Models/Order.cs
@@ -9,7 +9,16 @@ namespace Team1_ESNET_CA.Models
     {
         //Model get;set; for Order Controller/View
         //Parameters we need
-        /*Product Iamge, Product Name, Product Desc, Order Date, Order Quantity, Act Code.
-         * KIV Download URL LINK */
+        //Check if anisha if we are doing LINQ or EntityFrameWork
+        //Way of extracting data and creating model will be different. 
+        public string ProductImg { get; set;}
+        public string ProductName { get; set;}
+        public string ProductDesc { get; set;}
+        public string OrderDate { get;set;}
+        public int OrderQuantity { get; set;}
+        public string ActivationCode { get; set;}
+        public string SessionId { get;set;}
+        public string Email { get;set;}
+    }
     }
 }
