b10d3eb33d1123a71783c09c090d3f90d7dc483d
OperaOfLife <anishasinha10@gmail.com>
2021-04-10 19:08:36 +0800

    merged

--- Team1_ESNET_CA/Controllers/OrderController.cs
+++ Team1_ESNET_CA/Controllers/OrderController.cs
@@ -18,29 +18,29 @@ namespace Team1_ESNET_CA.Controllers
         public IActionResult Index()
         {
             //Validate the right user before display the details
-            /* string sessionId = Request.Cookies["sessionId"];
-             if (sessionId != null)
-             {*/
-            //Validate sessionId with User's sessionID
-            //Check with Anisha if we are going to put in cookies
-            //Confirm with Anisha if there is gonna be a central model for access to all models (eg. AppData)
-            /*Customers customer = Customers.Find(x => x.sessionId == sessionId);
-            if (sessionId == null)
+            string sessionId = Request.Cookies["sessionId"];
+            if (sessionId != null)
             {
-                Console.WriteLine("Invalid Transaction, Please Login again");
-                return RedirectToAction("Index", "Login");
+                //Validate sessionId with User's sessionID
+                //Check with Anisha if we are going to put in cookies
+                //Confirm with Anisha if there is gonna be a central model for access to all models (eg. AppData)
+                Customers customer = Customers.Find(x => x.sessionId == sessionId);
+                if (sessionId == null)
+                {
+                    Console.WriteLine("Invalid Transaction, Please Login again");
+                    return RedirectToAction("Index", "Login");
+                }
+                //Extract image data from OrderData after validation
+                ViewData["OrderImg"] = orderdata.ProductImg;
+                ViewData["OrderName"] = orderdata.ProductName;
+                ViewData["OrderDesc"] = orderdata.ProductDesc;
+                ViewData["OrderDate"] = orderdata.Orderdate;
+                ViewData["OrderNum"] = orderdata.OrderQuantity;
+                ViewData["ActCode"] = orderdata.ActivationCode;
+                ViewData["sessionId"] = sessionId;
             }
-            //Extract image data from OrderData after validation
-            ViewData["OrderImg"] = orderdata.ProductImg;
-            ViewData["OrderName"] = orderdata.ProductName;
-            ViewData["OrderDesc"] = orderdata.ProductDesc;
-            ViewData["OrderDate"] = orderdata.Orderdate;
-            ViewData["OrderNum"] = orderdata.OrderQuantity;
-            ViewData["ActCode"] = orderdata.ActivationCode;
-            ViewData["sessionId"] = sessionId;
-        }
 
-             return View("Index", "Order");*/
+            return View("Index", "Order");
 
             return View();
         }
