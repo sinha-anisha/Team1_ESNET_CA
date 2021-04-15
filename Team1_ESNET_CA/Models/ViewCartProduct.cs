322133abcb0c66542162bfeaf3216906c05f37e8
XueRong <xuerongzhang77@gmail.com>
2021-04-15 16:29:49 +0800

    I added model, controller, data

---
+++ Team1_ESNET_CA/Models/ViewCartProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team1_ESNET_CA.Models
+{
+    public class ViewCartProduct
+    {
+        public string productNmae { get; set; }
+        public string productImage { get; set; }
+        public string productDescription { get; set; }
+        public double unitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
