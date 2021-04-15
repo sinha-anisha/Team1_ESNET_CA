c5500de649a4a7feb23c335aa23085d89e5da067
OperaOfLife <anishasinha10@gmail.com>
2021-04-16 00:04:51 +0800

    ok

--- Team1_ESNET_CA/Models/ViewCartProduct.cs
+++ Team1_ESNET_CA/Models/ViewCartProduct.cs
@@ -6,13 +6,11 @@ using System.Threading.Tasks;
 namespace Team1_ESNET_CA.Models
 {
     public class ViewCartProduct
-    {   
-        public int productId { get; set; }
+    {
         public string productNmae { get; set; }
         public string productImage { get; set; }
         public string productDescription { get; set; }
         public double unitPrice { get; set; }
         public int Quantity { get; set; }
-        public int CartId { get; set; }
     }
 }
